4c4d485d2eb96e2afee99589ba5d0cf6ee491f95
Freddie Hill <freddiehill000@gmail.com>
2026-08-22 05:03:52 +0100

    add adjustable cull ring gizmo around castle

---
+++ Assets/Scripts/Editor/CullRingGizmo.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CullRingGizmo : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+    }
+}
